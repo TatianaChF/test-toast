7ecccc258044c7ee9742d37347abec8c2a974589
TatianaChF <tatiana.chefox@gmail.com>
2026-02-13 13:14:15 +0300

    add functionality to addToast

--- src/context/ToastContext.tsx
+++ src/context/ToastContext.tsx
@@ -1,12 +1,16 @@
 import React, { useState, type ReactNode } from 'react';
 import type { Toast } from '../types/types';
+import { v4 as uuid4 } from 'uuid';
 
 
 export const ToastProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [toasts, setToasts] = useState<Toast[]>([]);
 
   const addToast = (toast: Omit<Toast, 'id'>) => {
-    
+    const id: string = uuid4();
+    const newToast: Toast = {...toast, id};
+
+    setToasts((prevToasts) => [...prevToasts, newToast]);
   };
 
   const removeToast = (id: string) => {
