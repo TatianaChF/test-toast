3e7eb96e447c29074e19a0ce2260a486b3a3996d
TatianaChF <tatiana.chefox@gmail.com>
2026-02-13 19:52:04 +0300

    separat the context and the provider

--- src/context/ToastContext.tsx
+++ src/context/ToastContext.tsx
@@ -1,29 +1,14 @@
-import React, { useState, type ReactNode } from 'react';
-import type { Toast } from '../types/types';
-import { v4 as uuid4 } from 'uuid';
+import {useContext, createContext} from 'react';
+import type {ToastContextType} from '../types/types';
 
-
-export const ToastProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [toasts, setToasts] = useState<Toast[]>([]);
-
-  const addToast = (toast: Omit<Toast, 'id'>) => {
-    const id: string = uuid4();
-    const newToast: Toast = {...toast, id};
-
-    setToasts((prevToasts) => [...prevToasts, newToast]);
-  };
-
-  const removeToast = (id: string) => {
-    
-  };
-
-  return (
-   <>
-
-   </>
-  );
-};
+export const ToastContext = createContext<ToastContextType | null>(null);
 
 export const useToast = () => {
-  
+  const context = useContext(ToastContext);
+
+  if (!context) {
+    throw new Error('useToast must be used within a ToastProvider');
+  }
+
+  return context;
 };
---
+++ src/context/ToastProvider.tsx
@@ -0,0 +1,25 @@
+import { v4 as uuid4 } from 'uuid';
+import {type ReactNode, useState} from "react";
+import type {Toast} from "../types/types.ts";
+import {ToastContext} from "./ToastContext.tsx";
+
+export const ToastProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+    const [toasts, setToasts] = useState<Toast[]>([]);
+
+    const addToast = (toast: Omit<Toast, 'id'>) => {
+        const id: string = uuid4();
+        const newToast: Toast = {...toast, id};
+
+        setToasts((prevToasts) => [...prevToasts, newToast]);
+    };
+
+    const removeToast = (id: string) => {
+
+    };
+
+    return (
+        <ToastContext.Provider value={{ toasts, addToast, removeToast }}>
+            {children}
+        </ToastContext.Provider>
+    );
+};
