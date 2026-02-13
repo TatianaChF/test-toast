06fc0c2a3f894de66617cda7eabe3ced6c546c02
TatianaChF <tatiana.chefox@gmail.com>
2026-02-13 21:32:05 +0300

    add toast disappearance logic

--- src/components/ToastItem.tsx
+++ src/components/ToastItem.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import type { Toast } from '../types/types';
+import React, {useEffect} from 'react';
+import type {Toast} from '../types/types';
 
 interface ToastItemProps {
-  toast: Toast;
-  onRemove: (id: string) => void;
+    toast: Toast;
+    onRemove: (id: string) => void;
 }
 
-export const ToastItem: React.FC<ToastItemProps> = ({ toast, onRemove }) => {
-  return (
-    <div className={`toast toast-${toast.type}`}>
-      <span>{toast.message}</span>
-      <button onClick={() => onRemove(toast.id)}>x</button>
-    </div>
-  );
+export const ToastItem: React.FC<ToastItemProps> = ({toast, onRemove}) => {
+    useEffect(() => {
+        const interval = setInterval(() => {
+            onRemove(toast.id);
+        }, toast.duration);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className={`toast toast-${toast.type}`}>
+            <span>{toast.message}</span>
+            <button onClick={() => onRemove(toast.id)}>x</button>
+        </div>
+    );
 };
